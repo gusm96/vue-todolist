1292be2bbb46b855bdb102afea0a5415ba400854
Seongmo <gusm96@naver.com>
2022-12-05 17:58:50 +0900

    update 221205

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,28 +1,37 @@
 <template>
-    <main>
-
-    </main>
+  <main>
+    <div>
+      <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" />
+      <button v-on:click="addTodo">Submit</button>
+    </div>
+  </main>
 </template>
 <script>
 export default {
-    name: 'TodoInput',
-    components: {},
-    data() {
-        return {
-            example: '',
+  name: "TodoInput",
+  components: {},
+  data() {
+    return {
+      newTodoItem: ""
+    };
+  },
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeUnmount() {},
+  unmounted() {},
+  methods: {
+    addTodo: function() {
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput: function() {
+      this.newTodoItem = "";
     }
-},
-    beforeCreate() {},
-    created() {},
-    beforeMount() {},
-    mounted() {},
-    beforeUpdate() {},
-    updated() {},
-    beforeUnmount() {},
-    unmounted() {},
-    methods: {}
-}
+  }
+};
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
